782d85ff5e1e76315b5eef06c22beb463d36b073
Blake <108043930+h0hx@users.noreply.github.com>
2025-08-27 18:05:51 -0400

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class SnakeForm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SnakeForm1))
+        LabelTitle = New Label()
+        ExitButton = New Button()
+        AccountBox = New TextBox()
+        SearchButton = New Button()
+        DisplayNameBox = New TextBox()
+        SuspendLayout()
+        ' 
+        ' LabelTitle
+        ' 
+        LabelTitle.AutoSize = True
+        LabelTitle.Font = New Font("MS Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        LabelTitle.ForeColor = Color.Maroon
+        LabelTitle.Location = New Point(12, 9)
+        LabelTitle.Name = "LabelTitle"
+        LabelTitle.Size = New Size(47, 16)
+        LabelTitle.TabIndex = 0
+        LabelTitle.Text = "Snake"
+        ' 
+        ' ExitButton
+        ' 
+        ExitButton.FlatStyle = FlatStyle.Flat
+        ExitButton.Font = New Font("MS Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ExitButton.ForeColor = Color.DarkRed
+        ExitButton.Location = New Point(488, 8)
+        ExitButton.Name = "ExitButton"
+        ExitButton.Size = New Size(47, 26)
+        ExitButton.TabIndex = 1
+        ExitButton.Text = "Exit"
+        ExitButton.UseVisualStyleBackColor = True
+        ' 
+        ' AccountBox
+        ' 
+        AccountBox.BackColor = Color.Black
+        AccountBox.BorderStyle = BorderStyle.FixedSingle
+        AccountBox.ForeColor = Color.Maroon
+        AccountBox.Location = New Point(12, 40)
+        AccountBox.Multiline = True
+        AccountBox.Name = "AccountBox"
+        AccountBox.Size = New Size(523, 23)
+        AccountBox.TabIndex = 2
+        AccountBox.Text = "Account:"
+        ' 
+        ' SearchButton
+        ' 
+        SearchButton.BackColor = Color.Black
+        SearchButton.FlatStyle = FlatStyle.Flat
+        SearchButton.Font = New Font("MS Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        SearchButton.ForeColor = Color.Silver
+        SearchButton.Location = New Point(12, 69)
+        SearchButton.Name = "SearchButton"
+        SearchButton.Size = New Size(523, 27)
+        SearchButton.TabIndex = 4
+        SearchButton.Text = "Search User"
+        SearchButton.UseVisualStyleBackColor = False
+        ' 
+        ' DisplayNameBox
+        ' 
+        DisplayNameBox.BackColor = Color.Black
+        DisplayNameBox.BorderStyle = BorderStyle.FixedSingle
+        DisplayNameBox.ForeColor = Color.White
+        DisplayNameBox.Location = New Point(12, 102)
+        DisplayNameBox.Multiline = True
+        DisplayNameBox.Name = "DisplayNameBox"
+        DisplayNameBox.ReadOnly = True
+        DisplayNameBox.Size = New Size(523, 239)
+        DisplayNameBox.TabIndex = 5
+        ' 
+        ' SnakeForm1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Black
+        ClientSize = New Size(547, 353)
+        Controls.Add(DisplayNameBox)
+        Controls.Add(SearchButton)
+        Controls.Add(AccountBox)
+        Controls.Add(ExitButton)
+        Controls.Add(LabelTitle)
+        FormBorderStyle = FormBorderStyle.None
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "SnakeForm1"
+        Text = "Snake"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents LabelTitle As Label
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents AccountBox As TextBox
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents DisplayNameBox As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,79 @@
+﻿Imports System.Net.Http
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+Public Class SnakeForm1
+
+    <DllImport("user32.dll")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+
+    <DllImport("user32.dll")>
+    Private Shared Function SendMessage(hWnd As IntPtr, Msg As Integer, wParam As Integer, lParam As Integer) As Integer
+    End Function
+
+    Private Const WM_NCLBUTTONDOWN As Integer = &HA1
+    Private Const HTCAPTION As Integer = 2
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Close()
+    End Sub
+
+    Private Sub SnakeForm1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Async Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        For Each username As String In AccountBox.Lines
+            username = username.Trim()
+            If username = "" OrElse username.StartsWith("account:", StringComparison.OrdinalIgnoreCase) Then
+                Continue For
+            End If
+        Next
+
+
+        Dim usernames() As String = DisplayNameBox.Lines
+        DisplayNameBox.Clear()
+        DisplayNameBox.Text = "Checking..." & Environment.NewLine
+
+        Dim handler As New HttpClientHandler() With {
+            .AllowAutoRedirect = True
+        }
+
+        Using client As New HttpClient()
+            client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
+            For Each username As String In usernames
+
+                username = username.Trim()
+                If username = "" Then Continue For
+
+
+                Try
+
+                    Dim url As String = "https://www.instagram.com/api/v1/users/web_profile_info/?username=" & username
+                    Dim response As HttpResponseMessage = Await client.GetAsync(url)
+                    Dim body As String = Await response.Content.ReadAsStringAsync()
+
+
+                    If body.Contains("""user"":") Then
+                        DisplayNameBox.AppendText(username & " → ✔ Exists" & Environment.NewLine)
+                    Else
+                        DisplayNameBox.AppendText(username & " → ❌ Not found" & Environment.NewLine)
+                    End If
+
+                Catch ex As Exception
+                    DisplayNameBox.AppendText(username & " → Error: " & ex.Message & Environment.NewLine)
+                End Try
+            Next
+        End Using
+    End Sub
+
+    Private Sub SnakeForm1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        If e.Button = MouseButtons.Left Then
+            ReleaseCapture()
+            SendMessage(Me.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0)
+        End If
+    End Sub
+End Class
